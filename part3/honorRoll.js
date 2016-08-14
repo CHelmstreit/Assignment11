fca04867a07c72f96a2dcbeab55b6e8a316501fd
CHelmstreit <chelmstreit@gmail.com>
2016-08-14 00:05:23 -0700

    First Commit

---
+++ part3/honorRoll.js
@@ -0,0 +1,40 @@
+{
+    "ridgemontHighHonorRoll": [
+        {
+            "name": "Jeff Spicoli",
+            "grade": "Junior",
+            "gender": "M",
+            "age": "16"
+        },
+        {
+            "name": "Linda Barrett",
+            "grade": "Senior",
+            "gender": "F",
+            "age": "18"
+        },
+        {
+            "name": "Brad Hamilton",
+            "grade": "Senior",
+            "gender": "M",
+            "age": "18"
+        },
+        {
+            "name": "Stacy Hamilton",
+            "grade": "Sophomore",
+            "gender": "F",
+            "age": "15"
+        },
+        {
+            "name": "Charles Jefferson",
+            "grade": "Senior",
+            "gender": "M",
+            "age": "18"
+        },
+        {
+            "name": "Mike Damone",
+            "grade": "Senior",
+            "gender": "M",
+            "age": "18"
+        }
+]
+}
